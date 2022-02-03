2266fe361f3d1d42170b8ddcdc218a7dd5815e45
b1mind <brent@1mindsolutions.com>
2022-02-03 09:59:33 -0700

    📁 moved project files over

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
-};
+import adapter from '@sveltejs/adapter-node'
+import preprocess from 'svelte-preprocess'
+import autoprefixer from 'autoprefixer'
+import postcssCombineMediaQuery from 'postcss-combine-media-query'
 
-export default config;
+const config = {
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    //defaults for adapter just so I learn them
+    adapter: adapter(),
+  },
+  preprocess: [
+    preprocess({
+      postcss: { plugins: [autoprefixer(), postcssCombineMediaQuery()] },
+    }),
+  ],
+}
+
+export default config

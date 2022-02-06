ef5311c863ea3ea79e172d125d004736408d743f
b1mind <brent@1mindsolutions.com>
2022-02-06 02:15:15 -0700

    🧪 Alert testing working 🔧 Needs timer | GSAP timeline ⚠ Remove checkboxes (need prod check)

--- src/routes/stream/index.svelte
+++ src/routes/stream/index.svelte
@@ -1,6 +1,6 @@
 <script>
   import { browser } from '$app/env'
-  import { fly } from 'svelte/transition'
+  import { fly, slide } from 'svelte/transition'
   import Modes from '$lib/component/Modes.svelte'
 
   //todo handle isMode better: Maybe object for names/props?
@@ -12,13 +12,13 @@
 
   let raid = {
     user: 'user',
-    users: 69,
+    viewers: 100,
   }
 
   let subscriber = {
     user: 'testLongLongSubName',
     subType: 'subscribed',
-    subTime: '69months - 6month streak',
+    msg: '69 months - 6 month streak',
   }
 
   if (browser) {
@@ -31,6 +31,13 @@
       console.dir(viewers)
       console.dir(user)
       console.dir(extra)
+
+      raid = {
+        user: user,
+        viewers: viewers,
+      }
+
+      isRaid = true
     }
 
     ComfyJS.onSub = (user, message, subTierInfo, extra) => {
@@ -38,6 +45,14 @@
       console.dir(user)
       console.dir(subTierInfo)
       console.dir(extra)
+
+      subscriber = {
+        user: user,
+        subType: subTierInfo,
+        msg: 'message',
+      }
+
+      isSub = true
     }
 
     ComfyJS.onCommand = (user, command, message, flags, extra) => {
@@ -50,7 +65,7 @@
         subscriber = {
           user: user,
           subType: 'subscribed',
-          subTime: 'subTierInfo',
+          msg: 'subTierInfo',
         }
 
         isSub = true
@@ -77,41 +92,83 @@
 </script>
 
 <main>
+  <div class="test-controls">
+    <label for="isSub">
+      SubTest
+      <input bind:checked={isSub} type="checkbox" name="isSub" />
+    </label>
+
+    <label for="isRaid">
+      RaidTest
+      <input bind:checked={isRaid} type="checkbox" name="isRaid" />
+    </label>
+  </div>
+
   <div class="modes">
     <Modes active={isChat} modeType="chat" />
     <Modes active={isGhost} modeType="ghost" />
     <Modes active={isFocus} modeType="focus" />
   </div>
 
-  <article translate={fly} class="alerts">
+  <section class="alerts-wrap">
     {#if isSub}
-      <b>{subscriber.subType}</b>
-      <header>
-        <h2>
-          {subscriber.user}
-        </h2>
-        <i>{subscriber.subTime}</i>
-      </header>
+      <b in:slide={{ y: '-1rem', delay: 1000 }}>{subscriber.subType}</b>
+      <article in:fly={{ x: 400, duration: 550 }} class="alerts">
+        <header>
+          <h2>
+            {subscriber.user}
+          </h2>
+          <i>{subscriber.msg}</i>
+        </header>
 
-      <img
-        src="https://c.tenor.com/4g2pk3Cgf70AAAAC/mugen-pt.gif"
-        alt="Mugen Hiding from Tengu into Field"
-      />
+        <img
+          src="https://media.giphy.com/media/p7QJSVvU4bMWc/giphy.gif"
+          alt="Ed & Ein Excited Thank You"
+        />
+
+        <figure>
+          <img src="https://media.giphy.com/media/Y6QIbWeGMypig/giphy.gif" alt="" />
+          <figcaption>Follow</figcaption>
+        </figure>
+
+        <!-- todo make a end stream scene with this -->
+        <!-- <img
+          src="https://media.giphy.com/media/D05oEJk20L09a/giphy.gif"
+          alt="Ed riding into the sunset with pin wheel in background"
+        /> -->
+
+        <!-- todo use for raid? -->
+      </article>
     {:else if isRaid}
-      <header>
-        <h2>{raid.user} - {raid.users}</h2>
-      </header>
+      <b in:slide={{ y: '-1rem', delay: 1000 }}>raid</b>
+      <article in:fly={{ x: 400, duration: 550 }} class="alerts">
+        <header>
+          <h2>{raid.user}</h2>
+          <i>Raided with a party of {raid.viewers} viewers </i>
+        </header>
 
-      <!-- <img
-        src="https://media.giphy.com/media/D05oEJk20L09a/giphy.gif"
-        alt="Ed from Cowboy Bebop watering the garden"
-      /> -->
-      <img
-        src="https://c.tenor.com/dSPOmUJ6_t4AAAAC/mugen-samurai.gif"
-        alt="Mugen only needs his hands"
-      />
+        <img
+          src="https://c.tenor.com/4g2pk3Cgf70AAAAC/mugen-pt.gif"
+          alt="Mugen Hiding from Tengu into Field"
+        />
+
+        <img
+          src="https://media.giphy.com/media/aZzXDWIjefE5y/giphy.gif"
+          alt="Ed from Cowboy Bebop watering the garden"
+        />
+
+        <img
+          src="https://media.giphy.com/media/z9pV5qD0KEca4/giphy.gif"
+          alt="Fey hallucinating in the bathroom swimming with fishes"
+        />
+        <!-- todo make for keyboard only gif -->
+        <!-- <img
+          src="https://c.tenor.com/dSPOmUJ6_t4AAAAC/mugen-samurai.gif"
+          alt="Mugen only needs his hands to code"
+        /> -->
+      </article>
     {/if}
-  </article>
+  </section>
 
   {#if isChat}
     <p in:fly>Chat Mode: Working on something and talking with chat.</p>
@@ -129,6 +186,7 @@
 <style lang="scss">
   h1,
   h2,
+  figure,
   p {
     margin: 0;
   }
@@ -136,7 +194,7 @@
   main {
     height: 100%;
     display: grid;
-    grid-template-columns: 420px 1fr 296px 154px;
+    grid-template-columns: 420px 1fr 296px 153px;
     grid-template-rows: 1fr 420px 58px 92px;
     grid-template-areas:
       '. . . modes'
@@ -170,37 +228,52 @@
     place-content: center;
   }
 
-  .alerts {
+  .alerts-wrap {
     grid-area: alerts;
     place-self: end;
+    min-width: 296px;
     display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr;
     overflow: hidden;
 
     b {
-      grid-area: 1/1;
       justify-self: end;
+      padding: 0.35rem 0.65rem;
+      color: var(--clr-primary-bg);
+      text-transform: uppercase;
+      font-weight: 800;
+      background-color: var(--clr-highlight);
+      z-index: 99;
     }
+  }
+
+  .alerts {
+    display: grid;
+    background-color: var(--clr-primary-bg);
 
     header {
-      padding: 0.65rem;
+      // grid-area: 1/1/2/2;
+      padding: 0.6rem;
       color: var(--clr-white);
-      background-color: var(--clr-secondary-bg);
+      text-align: right;
+      border: 9px solid var(--clr-secondary-bg);
+      background-color: var(--clr-primary-bg);
 
       h2 {
+        margin-bottom: 0.25rem;
+        color: var(--clr-active);
         font-size: 1.4rem;
       }
     }
 
     img {
+      justify-self: center;
       display: block;
       max-width: 100%;
       height: 220px;
       object-fit: cover;
       // max-width: 292px;
       // aspect-ratio: 16/9;
-      border: 7px solid var(--clr-primary-bg);
+      border: 9px solid var(--clr-primary-bg);
     }
   }
 </style>

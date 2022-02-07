5aefb0d16f1ce1b0eed7ad3041f32991c13bd20e
b1mind <brent@1mindsolutions.com>
2022-02-06 23:13:21 -0700

    🧪 API post test for twitch cli

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,8 +2,7 @@ export async function get({ params, fetch, request }) {
   //todo get response from twitch-cli server https://localhost:9000/eventSub *
   //CORs issue? 500 response?
 
-  console.log(params)
-  console.dir(fetch)
+  console.log(params, 'params')
   console.dir(request)
   const data = request.json
   console.dir(data)
@@ -12,5 +11,21 @@ export async function get({ params, fetch, request }) {
 
   return {
     body: request,
+  }
+}
+
+export async function post({ params, fetch, request }) {
+  //todo get response from twitch-cli server https://localhost:9000/eventSub *
+  //CORs issue? 500 response?
+
+  console.log(params, 'params')
+  console.dir(request)
+  const data = request.json
+  console.dir(data)
+
+  console.dir(request.headers)
+
+  return {
+    body: request.json,
   }
 }

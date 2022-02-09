91e0863fb72907b11454fff252681461eee27104
b1mind <brent@1mindsolutions.com>
2022-02-09 00:30:22 -0700

    ⌨ Keyboard page made, needs captions/text

--- src/routes/stream/keeb.svelte
+++ src/routes/stream/keeb.svelte
@@ -1,1 +1,56 @@
-testing
+<figure>
+  <img src="/img/keeb/kit-in-bags.jpg" alt="Keyboard build kit: In bags" />
+  <figcaption />
+</figure>
+
+<figure>
+  <img src="/img/keeb/kit-in-bags2.jpg" alt="Keyboard build kit: In bags alternative" />
+  <figcaption />
+</figure>
+
+<figure>
+  <img
+    src="/img/keeb/pcb-with-resistors.jpg"
+    alt="Keyboard build kit: resistors soldered on pcb"
+  />
+  <figcaption />
+</figure>
+
+<figure>
+  <img
+    src="/img/keeb/hot-swapable-install.jpg"
+    alt="Keyboard build kit: swapable switch sockets"
+  />
+  <figcaption />
+</figure>
+
+<figure>
+  <img
+    src="/img/keeb/promicros-installed.jpg"
+    alt="Keyboard build kit: soldered in promicros"
+  />
+  <figcaption />
+</figure>
+
+<figure>
+  <img
+    src="/img/keeb/almost-complete.jpg"
+    alt="Almost complete with case assembled and some switches installed"
+  />
+  <figcaption />
+</figure>
+
+<figure>
+  <img
+    src="/img/keeb/finished-keyboard.jpg"
+    alt="Finished keyboard sitting on the desk with mouse"
+  />
+  <figcaption />
+</figure>
+
+<style lang="scss">
+  img {
+    max-width: 100%;
+    object-fit: cover;
+  }
+</style>

2585f1108b7fc1b015e421dbb31ed6fefb6f8fa6
b1mind <brent@1mindsolutions.com>
2022-02-14 11:11:54 -0700

    🐣 start of intro/outro scenes

---
+++ src/routes/stream/[scene].svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  export async function load({ params, url }) {
+    return {
+      props: {
+        params: params,
+      },
+    }
+  }
+</script>
+
+<script>
+  export let params
+</script>
+
+<main>
+  <h1>{params.scene}</h1>
+  <b>{timer}</b>
+</main>
+
+<style lang="scss">
+</style>
